44c0e9848e64ed016f1f65d6b3c8f02cfdf2a259
na10run <na10run@gmail.com>
2015-09-15 09:09:12 +0545

    models

--- Ecommerce/Pasal/models.py
+++ Ecommerce/Pasal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    id = models.IntegerField()
+    product_name = models.CharField(max_length=30)
+    product_code= models.CharField(max_length=30)
+    price = models.IntegerField()
+    available_quantity = models.IntegerField()
+    created_date = models.DateField()
+    updated_date = models.DateField() 
